41ad8d9c15446cfc58a9cd4ac4096f7642833b54
Leonel <franciscoleonelfds@gmail.com>
2024-05-21 22:47:37 -0300

    Efeito degitação nas páginas html.

--- menuMobile.js
+++ menuMobile.js
@@ -1,11 +1,11 @@
-const botaoMenu = document.querySelector('.botao_menu');
-const menuMobile = document.querySelector('.menu-navegacao-mobile');
+const botaoMenu = document.querySelector(".botao_menu");
+const menuMobile = document.querySelector(".menu-navegacao-mobile");
 let menuAberto = false;
 
-botaoMenu.addEventListener('click', function () {
-    menuAberto ? menuMobile.classList.add('invisible') : menuMobile.classList.remove('invisible');
+botaoMenu.addEventListener("click", function () {
+    menuAberto ? menuMobile.classList.add("invisible") : menuMobile.classList.remove("invisible");
 
-    menuAberto ? botaoMenu.children[0].setAttribute('src', 'images/icon_burger.svg') : botaoMenu.children[0].setAttribute('src', 'images/close_icon.svg');
+    menuAberto ? botaoMenu.children[0].setAttribute("src", "images/icon_burger.svg") : botaoMenu.children[0].setAttribute("src", "images/close_icon.svg");
 
     menuAberto = !menuAberto;
 });
