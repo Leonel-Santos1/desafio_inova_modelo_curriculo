64174c562bce26c9359616f2680e6c66b3e9bffc
Leonel <franciscoleonelfds@gmail.com>
2024-05-22 07:31:46 -0300

    Mudanças na posição do menu.

--- menuMobile.js
+++ menuMobile.js
@@ -1,11 +1,15 @@
 const botaoMenu = document.querySelector(".botao_menu");
 const menuMobile = document.querySelector(".menu-navegacao-mobile");
+var apresentacao = document.querySelector(".apresentacao");
+
 let menuAberto = false;
 
 botaoMenu.addEventListener("click", function () {
     menuAberto ? menuMobile.classList.add("invisible") : menuMobile.classList.remove("invisible");
 
     menuAberto ? botaoMenu.children[0].setAttribute("src", "images/icon_burger.svg") : botaoMenu.children[0].setAttribute("src", "images/close_icon.svg");
+
+    menuAberto ? apresentacao.setAttribute("style", "padding-top: 12%") : apresentacao.setAttribute("style", "padding-top: 7%");
 
     menuAberto = !menuAberto;
 });
